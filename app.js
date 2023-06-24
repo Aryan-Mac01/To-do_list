5f965a52da7a9590f0f9ed27e93586422a47f6bc
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-06-24 06:05:44 +0530

    add your own task

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   var today = new Date();
@@ -21,9 +25,13 @@ app.get("/", function(req, res) {
   
 
 
-  res.render("list", {
-    kindOfDay: day
-  });
+  res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req,res){
+  var item = (req.body.newItem);
+  items.push(item)
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
